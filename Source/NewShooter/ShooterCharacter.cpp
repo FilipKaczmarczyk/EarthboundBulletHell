a8aca33c0cd36a86915b477585e848fc1ebaa796
FilipKaczmarczyk <stifmeister0700@gmail.com>
2024-01-04 11:23:06 +0100

    Initial commit

---
+++ Source/NewShooter/ShooterCharacter.cpp
@@ -0,0 +1,146 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 300.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AssignPlayerController();
+
+	if (PlayerController)
+	{
+		// Get the Enhanced Input Local Player Subsystem from the Local Player related to our Player Controller.
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// PawnClientRestart can run more than once in an Actor's lifetime, so start by clearing out any leftover mappings.
+			Subsystem->ClearAllMappings();
+
+			// Add each mapping context, along with their priority values. Higher values outprioritize lower values.
+			Subsystem->AddMappingContext(DefaultInputMappingContext, DefaultInputMappingPriority);
+		}
+	}
+}
+
+void AShooterCharacter::AssignPlayerController()
+{
+	if (!PlayerController)
+	{
+		PlayerController = Cast<APlayerController>(GetController());
+	}
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	if (UEnhancedInputComponent* PlayerEnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveForwardAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &AShooterCharacter::MoveForward);
+		}
+
+		if (MoveRightAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &AShooterCharacter::MoveRight);
+		}
+		
+		if (TurnRateAction)
+        {
+        	PlayerEnhancedInputComponent->BindAction(TurnRateAction, ETriggerEvent::Triggered, this, &AShooterCharacter::TurnAtRate);
+        }
+        		
+		if (LookUpRateAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(LookUpRateAction, ETriggerEvent::Triggered, this, &AShooterCharacter::LookUpAtRate);
+		}
+
+		if (TurnAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(TurnAction, ETriggerEvent::Triggered, this, &AShooterCharacter::Turn);
+		}
+        		
+		if (LookUpAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(LookUpAction, ETriggerEvent::Triggered, this, &AShooterCharacter::LookUp);
+		}
+
+		if (JumpAction)
+		{
+			PlayerEnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &AShooterCharacter::Jump);
+		}
+	}
+}
+
+void AShooterCharacter::MoveForward(const FInputActionValue& Value)
+{
+	FVector DeltaLocation = FVector::ZeroVector;
+	DeltaLocation.X = Value[0];
+	
+	AddActorLocalOffset(DeltaLocation * 500.f * UGameplayStatics::GetWorldDeltaSeconds(this), true);
+}
+
+void AShooterCharacter::MoveRight(const FInputActionValue& Value)
+{
+	FVector DeltaLocation = FVector::ZeroVector;
+	DeltaLocation.Y = Value[0];
+	
+	AddActorLocalOffset(DeltaLocation * 500.f * UGameplayStatics::GetWorldDeltaSeconds(this), true);
+}
+
+void AShooterCharacter::TurnAtRate(const FInputActionValue& Value)
+{
+	AddControllerYawInput(Value[0] * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::LookUpAtRate(const FInputActionValue& Value)
+{
+	AddControllerPitchInput(Value[0] * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AShooterCharacter::Turn(const FInputActionValue& Value)
+{
+	AddControllerYawInput(Value[0]);
+}
+
+void AShooterCharacter::LookUp(const FInputActionValue& Value)
+{
+	AddControllerPitchInput(Value[0]);
+}
+
+void AShooterCharacter::Jump(const FInputActionValue& Value)
+{
+	Super::Jump();
+}
+
+
---
+++ Source/NewShooter/ShooterCharacter.h
@@ -0,0 +1,93 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class NEWSHOOTER_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	APlayerController* PlayerController;
+
+	void AssignPlayerController();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputMappingContext* DefaultInputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	int32 DefaultInputMappingPriority = 0;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveForwardAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* MoveRightAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* TurnRateAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookUpRateAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* TurnAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* LookUpAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	UInputAction* JumpAction;
+
+	void MoveForward(const FInputActionValue& Value);
+
+	void MoveRight(const FInputActionValue& Value);
+
+	void TurnAtRate(const FInputActionValue& Value);
+
+	void LookUpAtRate(const FInputActionValue& Value);
+
+	void Turn(const FInputActionValue& Value);
+
+	void LookUp(const FInputActionValue& Value);
+
+	void Jump(const FInputActionValue& Value);
+
+private:
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
+	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
+	float BaseTurnRate = 15;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"));
+	float BaseLookUpRate = 15;
+public:
+	
+	AShooterCharacter();
+	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+
+	UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	APlayerController* GetPlayerController() const { return PlayerController; };
+};
